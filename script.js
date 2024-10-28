f201cfb48fba31c8c1a1186632d07d2bbcf49e81
Zato <zatoman@gmail.com>
2024-10-28 14:12:59 -0400

    Created functions to convert the time format and get the day of the week. Also, I saved data from each hour of hourly forecast in an array.

--- script.js
+++ script.js
@@ -1,86 +1,203 @@
+// Navigation variables
 const search = document.querySelector("#search_location");
 const savedLocations = document.querySelectorAll("saved_location");
-const savedCity = document.querySelector("sl_city");
-const savedTime = document.querySelector("sl_time");
-const savedTemp = document.querySelector("sl_temp");
-const savedSkyCondition = document.querySelector("sl_sky_condition");
-const savedHighTemp = document.querySelector("sl_high");
-const savedLowTemp = document.querySelector("sl_low");
 
+// Saved Locations variables
+const slCity = document.querySelector("sl_city");
+const slTime = document.querySelector("sl_time");
+const slTemp = document.querySelector("sl_temp");
+const slSkyCondition = document.querySelector("sl_sky_condition");
+const slHighTemp = document.querySelector("sl_high");
+const slLowTemp = document.querySelector("sl_low");
+
+// Main Weather variables
+const mwCity = document.querySelector("mw_city");
+const mwTemp = document.querySelector("mw_temperature");
+const mwSkyCondition = document.querySelector("mw_sky_condition");
+const mwFeelsLike = document.querySelector("mw_feels_like");
+const mwHighTemp = document.querySelector("mw_high");
+const mwLowTemp = document.querySelector("mw_low");
+
+// Hourly Forecast variables
+const hfTime = document.querySelector("hf_time");
+const hfSymbol = document.querySelector("hf_symbol");
+const hfTemp = document.querySelector("hf_temperature");
+const hfPrec = document.querySelector("hf_number");
+
+// Weekly Forecast variables
+const wfSymbol = document.querySelector("wf_symbol");
+const wfDay = document.querySelector("wf_day");
+const wfSkyCondition = document.querySelector("wf_sky_condition");
+const wfPrec = document.querySelector("wf_percentage");
+const wfHighTemp = document.querySelector("wf_high");
+const wfLowTemp = document.querySelector("wf_low");
+
+// Weather Conditions variables
+const windNumber = document.querySelectorAll("number")[0];
+const windArrow = document.getElementById("arrow");
+const humidityNumber = document.querySelectorAll("number")[1];
+const uviNumber = document.querySelectorAll("number")[2];
+const pressureNumber = document.querySelectorAll("number")[3];
+const sunriseNumber = document.querySelectorAll("number")[4];
+const sunsetNumber = document.querySelectorAll("number")[5];
+
+// Weather API variables
 const WEATHER_API_KEY = "2a0265e0ff8f4b72881145040242110";
 const WEATHER_API_BASE_URL = "https://api.weatherapi.com/v1";
 const CURRENT = "/current.json";
 const FORECAST = "/forecast.json";
-const HISTORY = "/history.json";
-const MARINE = "/marine.json";
-const ASTRONOMY = "/astronomy.json";
 
+// Array variables
+const savedLocationCollection = [];
+const savedLocationVariablesCollection = [slCity, slTime, slTemp, slSkyCondition, slHighTemp, slLowTemp, mwFeelsLike];
+const hourlyForecastCollection = [];
+const hourlyForecastVariablesCollection = [hfTime, hfSymbol, hfTemp, hfPrec];
+const weeklyForecastCollection = [];
+const weeklyForecastVariablesCollection = [wfSymbol, wfDay, wfSkyCondition, wfPrec, wfHighTemp, wfLowTemp];
+
+// Updates weather app on location search
 search.addEventListener("keydown", event => {
     if (event.key === "Enter") {
         if (search.value.trim() === "") {
             alert("An empty search field is not allowed!");
         } else {
             fetchWeather();
+            updateWeather();
         }
     }
 });
 
+// Fetches weather data and presents it to the user
 async function fetchWeather() {
-    const location = search.value;
-    const currentWeatherAPI = new URL(WEATHER_API_BASE_URL + CURRENT + "?key=" + WEATHER_API_KEY);
-    currentWeatherAPI.searchParams.set("q", location);
-    const forecastWeatherAPI = new URL(WEATHER_API_BASE_URL + FORECAST + "?key=" + WEATHER_API_KEY);
-    forecastWeatherAPI.searchParams.set("q", location);
-    forecastWeatherAPI.searchParams.set("days", 7);
-    const historyWeatherAPI = new URL(WEATHER_API_BASE_URL + HISTORY + "?key=" + WEATHER_API_KEY);
-    historyWeatherAPI.searchParams.set("q", location);
-    historyWeatherAPI.searchParams.set("dt", "2024-10-26");
-    const marineWeatherAPI = new URL(WEATHER_API_BASE_URL + MARINE + "?key=" + WEATHER_API_KEY);
-    marineWeatherAPI.searchParams.set("q", location);
-    marineWeatherAPI.searchParams.set("days", 7);
-    const astronomyWeatherAPI = new URL(WEATHER_API_BASE_URL + ASTRONOMY + "?key=" + WEATHER_API_KEY);
-    astronomyWeatherAPI.searchParams.set("q", location);
-    astronomyWeatherAPI.searchParams.set("dt", "2024-10-26");
-    
     try {
+        const location = search.value;
+        
+        // Weather API URL variables
+        const currentWeatherAPI = new URL(WEATHER_API_BASE_URL + CURRENT + "?key=" + WEATHER_API_KEY);
+        currentWeatherAPI.searchParams.set("q", location);
+        const forecastWeatherAPI = new URL(WEATHER_API_BASE_URL + FORECAST + "?key=" + WEATHER_API_KEY);
+        forecastWeatherAPI.searchParams.set("q", location);
+        forecastWeatherAPI.searchParams.set("days", 7);
+
+        // Fetches weather data
         const getCurrentWeather = await fetch(currentWeatherAPI);
         const getCurrentWeatherJSON = await getCurrentWeather.json();
         console.log(getCurrentWeatherJSON);
         const getForecastWeather = await fetch(forecastWeatherAPI);
         const getForecastWeatherJSON = await getForecastWeather.json();
         console.log(getForecastWeatherJSON);
-        const getHistoryWeather = await fetch(historyWeatherAPI);
-        const getHistoryWeatherJSON = await getHistoryWeather.json();
-        console.log(getHistoryWeatherJSON);
-        const getMarineWeather = await fetch(marineWeatherAPI);
-        const getMarineWeatherJSON = await getMarineWeather.json();
-        console.log(getMarineWeatherJSON);
-        const getAstronomyWeather = await fetch(astronomyWeatherAPI);
-        const getAstronomyWeatherJSON = await getAstronomyWeather.json();
-        console.log(getAstronomyWeatherJSON);
 
-        const search = document.querySelector("#search_location");
-        const savedLocations = document.querySelectorAll("saved_location");
-        const savedCity = document.querySelector("sl_city");
-        const fetchSavedCity = getCurrentWeatherJSON.location.name;
+        // Saves JSON weather data into variables and prints them out
+        console.log("SAVED LOCATIONS VARIABLES:")
+        const savedLocation = [];
+        const fetchSavedCity = getForecastWeatherJSON.location.name;
+        savedLocation.push(fetchSavedCity);
         console.log("City: " + fetchSavedCity);
-        const savedTime = document.querySelector("sl_time");
-        const fetchSavedTime = getCurrentWeatherJSON.location.localtime;
+        const fetchSavedTime = convertTimeFormat(getCurrentWeatherJSON.location.localtime.split(" ")[1]);
+        savedLocation.push(fetchSavedTime);
         console.log("Time: " + fetchSavedTime);
-        const savedTemp = document.querySelector("sl_temp");
-        const fetchSavedTemp = getCurrentWeatherJSON.current.temp_f;
+        const fetchSavedTemp = Math.round(getCurrentWeatherJSON.current.temp_f);
+        savedLocation.push(fetchSavedTemp);
         console.log("Temperature: " + fetchSavedTemp);
-        const savedSkyCondition = document.querySelector("sl_sky_condition");
         const fetchSavedSkyCondition = getCurrentWeatherJSON.current.condition.text;
+        savedLocation.push(fetchSavedSkyCondition);
         console.log("Sky Condition: " + fetchSavedSkyCondition);
-        const savedHighTemp = document.querySelector("sl_high");
-        const fetchSavedHighTemp = getForecastWeatherJSON.forecast.forecastday[0].day.maxtemp_f;
+        const fetchSavedHighTemp = Math.round(getForecastWeatherJSON.forecast.forecastday[0].day.maxtemp_f);
+        savedLocation.push(fetchSavedHighTemp);
         console.log("High: " + fetchSavedHighTemp);
-        const savedLowTemp = document.querySelector("sl_low");
-        const fetchSavedLowTemp = getForecastWeatherJSON.forecast.forecastday[0].day.mintemp_f;
+        const fetchSavedLowTemp = Math.round(getForecastWeatherJSON.forecast.forecastday[0].day.mintemp_f);
+        savedLocation.push(fetchSavedLowTemp);
         console.log("Low: " + fetchSavedLowTemp);
+        const fetchMainFeelsLike = Math.round(getCurrentWeatherJSON.current.feelslike_f);
+        savedLocation.push(fetchMainFeelsLike);
+        console.log("Feels like: " + fetchMainFeelsLike);
 
+        console.log("HOURLY FORECAST VARIABLES:");
+        const hfStart = Math.ceil(getCurrentWeatherJSON.location.localtime.split(" ")[1].split(":").join("."));
+        const hfLength = getForecastWeatherJSON.forecast.forecastday[0].hour.length;
+        const hfIntro = getForecastWeatherJSON.forecast.forecastday[0].hour;
+
+        for (let i = hfStart; i < hfLength + hfStart; i++) {
+            const hourlyForecast = [];
+            const fetchHFTime = convertTimeFormat(hfIntro[i % hfLength].time.split(" ")[1]);
+            hourlyForecast.push(fetchHFTime);
+            console.log("Hourly Time: " + fetchHFTime);
+            const fetchHFSkyCondition = hfIntro[i % hfLength].condition.text;
+            hourlyForecast.push(fetchHFSkyCondition);
+            console.log("Hourly Sky Condition: " + fetchHFSkyCondition);
+            const fetchHFTemp = Math.round(hfIntro[i % hfLength].temp_f);
+            hourlyForecast.push(fetchHFTemp);
+            console.log("Hourly Temperature: " + fetchHFTemp);
+            const fetchHFPrec = Math.round(Math.max(hfIntro[i % hfLength].chance_of_rain, hfIntro[i % hfLength].chance_of_snow));
+            hourlyForecast.push(fetchHFPrec);
+            console.log("Hourly Precipitation: " + fetchHFPrec);
+        }
+        
+        
+        console.log("WEEKLY FORECAST VARIABLES:");
+        const weeklyForecast = [];
+        const fetchWFDay = getDayOfTheWeek(getForecastWeatherJSON.forecast.forecastday[0].date);
+        weeklyForecast.push(fetchWFDay);
+        console.log("First Day - Day: " + fetchWFDay);
+        const fetchWFSkyCondition = getForecastWeatherJSON.forecast.forecastday[0].day.condition.text;
+        weeklyForecast.push(fetchWFSkyCondition);
+        console.log("First Day - Sky Condition: " + fetchWFSkyCondition);
+        const fetchWFPrec = Math.round(Math.max(getForecastWeatherJSON.forecast.forecastday[0].day.daily_chance_of_rain, getForecastWeatherJSON.forecast.forecastday[0].day.daily_chance_of_snow));
+        weeklyForecast.push(fetchWFPrec);
+        console.log("First Day - Precipitation: " + fetchWFPrec);
+        const fetchWFHighTemp = Math.round(getForecastWeatherJSON.forecast.forecastday[0].day.maxtemp_f);
+        weeklyForecast.push(fetchWFHighTemp);
+        console.log("First Day - High: " + fetchWFHighTemp);
+        const fetchWFLowTemp = Math.round(getForecastWeatherJSON.forecast.forecastday[0].day.mintemp_f);
+        weeklyForecast.push(fetchWFLowTemp);
+        console.log("First Day - Low: " + fetchWFLowTemp);
+
+        console.log("WEATHER CONDITIONS VARIBALES:");
+        const weatherConditions = [];
+        const fetchWindSpeed = getCurrentWeatherJSON.current.wind_mph;
+        weatherConditions.push(fetchWindSpeed);
+        const fetchWindDirection = getCurrentWeatherJSON.current.wind_dir;
+        weatherConditions.push(fetchWindDirection);
+        // make a function that turns the direction into rotation degrees for the arrow
+        console.log(`Wind: ${fetchWindSpeed}mph | Direction: ${fetchWindDirection}`);
+        const fetchHumidity = getCurrentWeatherJSON.current.humidity;
+        weatherConditions.push(fetchHumidity);
+        console.log(`Humidity: ${fetchHumidity}%`);
+        const fetchUVI = getCurrentWeatherJSON.current.uv;
+        weatherConditions.push(fetchUVI);
+        console.log(`UVI: ${fetchUVI}`);
+        const fetchPressure = getCurrentWeatherJSON.current.pressure_in;
+        weatherConditions.push(fetchPressure);
+        console.log(`Pressure: ${fetchPressure}inHg`);
+        const fetchSunriseTime = getForecastWeatherJSON.forecast.forecastday[0].astro.sunrise;
+        weatherConditions.push(fetchSunriseTime);
+        const fetchSunsetTime = getForecastWeatherJSON.forecast.forecastday[0].astro.sunset;
+        weatherConditions.push(fetchSunsetTime);
+        console.log(`Sunrise: ${fetchSunriseTime} | Sunset: ${fetchSunsetTime}`);
     } catch (error) {
         console.error("ERROR: " + error);
     }
+}
+
+// Converts from 24-hour time to 12-hour time
+function convertTimeFormat(originalTime) {
+    let [hours, minutes] = originalTime.split(":");
+    let unit = hours < 12 ? "AM" : "PM";
+    if (hours != 12 && hours != 0) {
+        hours = hours % 12;
+    } else {
+        hours = 12;
+    }
+    return `${hours}:${minutes} ${unit}`;
+}
+
+// Returns day of the week of a given date
+function getDayOfTheWeek(originalDate) {
+    let date = new Date(originalDate);
+    const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return daysOfTheWeek[date.getDay()];
+}
+
+function updateWeather() {
+
 }
